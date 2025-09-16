c95c5d25486a1a3cf3d97b7875766cb28f8c13eb
sahdan17 <sahdan.wira.17@gmail.com>
2025-09-16 09:01:33 +0700

    update

--- assets/folService-CJ7Ni6l7.js
+++
@@ -1,1 +0,0 @@
-import{p as a}from"./api-CormumB1.js";const o=async t=>(await a("/fe/getAllSpots",t,!0)).data,r=async t=>(await a("/fe/getDataBySpot",t,!0)).data;export{r as a,o as g};
---
+++ assets/folService-MCIdZNKB.js
@@ -0,0 +1,1 @@
+import{p as a}from"./api-tIjG661J.js";const o=async t=>(await a("/fe/getAllSpots",t,!0)).data,r=async t=>(await a("/fe/getDataBySpot",t,!0)).data;export{r as a,o as g};
